f5a2b346672287cc42893eb6bfc24e50a6e7bf6c
zlkfb <bessieres.fabien@gmail.com>
2018-01-11 22:38:28 +0100

    add .TTF et nouveaux pions et affichage dans t8.c

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,7 @@
 #gcc -o t4 -I/usr/include/SDL2 t4.c -lSDL2_image -lSDL2 -lpthread
 #gcc -o t5 -I/usr/include/SDL2 t5.c -lSDL2_image -lSDL2 -lpthread
 #gcc -o t6 -I/usr/include/SDL2 t6.c -lSDL2_image -lSDL2 -lpthread
-gcc -o t7 -I/usr/include/SDL2 t8.c -lSDL2_image -lSDL2 -lpthread
+gcc -o t7 -I/usr/include/SDL2 t8.c -lSDL2main -lSDL2_image -lSDL2 -lSDL_ttf -lfreetype -lpthread
 gcc -o pathfinding pathfinding.c
 gcc -o client client.c
 gcc -o server_white server_white.c
